fb39cf534c2fd699c1f6d0b4a6c2cc1f1dc923da
madajabbar <madajabbar22@gmail.com>
2023-05-02 18:18:02 +0700

    check edge

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -145,7 +145,7 @@ class HomeController extends Controller
             }
             // $arr = Access::whereIn('unique_key', $expld)->where('room_id', $ruangan_id)->where('day', Carbon::now()->format('l'))->where('start_at', '<', Carbon::now())->where('end_at', '>', Carbon::now())->first();
             $arr = Access::whereIn('unique_key', $expld)->where('room_id', $ruangan_id)->first();
-            dd($arr);
+            // dd($arr);
 
             if (is_null($arr)) {
                 return ResponseFormatter::error(null, 'Invalid QR');
